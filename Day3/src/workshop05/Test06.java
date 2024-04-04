f071c14ed9eb35b986ffbb32e5e110b24ff58035
wj0624 <wonjeong4538@gmail.com>
2024-04-04 13:34:18 +0900

    240404

---
+++ Day3/src/workshop05/Test06.java
@@ -0,0 +1,16 @@
+package workshop05;
+import java.util.*;
+import java.io.*;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		String str = "JAVAProgramming";
+		System.out.println(str.toUpperCase());
+		System.out.println(str.toLowerCase());
+		System.out.println(str.substring(0,4));
+		System.out.println(str.substring(4));
+		
+	}
+
+}
